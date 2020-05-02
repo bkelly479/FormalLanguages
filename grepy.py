ac6ce0e060b4bd942796913bbe216a4716ac1e5b
Brendan <brendan.kelly4@marist.edu>
2020-05-02 12:31:03 -0400

    reads from regex file

--- grepy.py
+++ grepy.py
@@ -1,1 +1,15 @@
-print("hello world!")
+#!/use/bin/env py -3
+
+import argparse
+
+#create commandline arguments 
+parser = argparse.ArgumentParser()
+parser.add_argument("-n")
+parser.add_argument("-d")
+parser.add_argument("REGEX")
+args = parser.parse_args()
+
+regexFile = open(args.REGEX, "r")
+
+
+print(regexFile.read())
